f3fde9d3d085ae0334e0172ff6f60ea1d70c8e4a
TiagoMarcos101 <Tiagomarcos204@gmail.com>
2022-01-21 09:40:44 -0100

    teste2

---
+++ Aplicacao/Aplicacao/Model/Dadosuser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aplicacao.Model
+{
+    public class Dadosuser
+    {
+         string Nome;
+         string Pass;
+        public Dadosuser(string Nome, string Pass)
+        {
+            this.Nome = Nome;
+            this.Pass = Pass;
+        }
+     
+        public string getNome() {
+            return Nome;
+        }
+        public void  SetNome(string Nome) {
+            this.Nome = Nome;
+        }
+        public string getPass()
+        {
+            return Pass;
+        }
+        public void SetPass(string Pass)
+        {
+            this.Nome = Pass;
+        }
+    }
+}
---
+++ Aplicacao/Aplicacao/vc/Pagenu.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aplicacao.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Aplicacao.VC
+{
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    public partial class Pagenu : ContentPage
+    {
+
+
+        public void GetData(Dadosuser U)
+        {
+
+        }
+
+        public Pagenu()
+        {
+            InitializeComponent();
+        }
+ 
+        async private void getdata(object sender, EventArgs e)
+        {
+            string pass = pw2.Text;
+            string fname = name2.Text;
+
+        
+                await Navigation.PushAsync(new Pagelogin(), true);
+            
+        }
+
+    }
+}
