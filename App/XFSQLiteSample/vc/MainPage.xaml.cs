c98009f4271b3656cbc5b36f92efa9136a33d44c
TiagoMarcos101 <Tiagomarcos204@gmail.com>
2022-02-07 18:57:04 -0100

    07 school

---
+++ App/XFSQLiteSample/vc/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XFSQLiteSample;
+namespace XFSQLiteSample.vc
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+        InitializeComponent();
+        }
+
+        async private void Paglogin(object sender, EventArgs e)
+        {
+
+           // await Navigation.PushAsync(new Pagenu(), true);
+        }
+        async private void Pagenubtn(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Pagenu(), true);
+        }
+        private void Pagenu_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
